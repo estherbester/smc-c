32257d8c1c87743eafbe2dfb77a14712f9328979
Esther N <esthernam@gmail.com>
2012-09-21 21:47:38 -0700

    Assignment 1

---
+++ assign1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+char getGrade(double testValue){
+
+	char grade = 'F';
+
+	
+	if (testValue >= 90.0)
+		grade = 'A';
+	else
+	    {
+		if (testValue >= 80.0)
+      		grade = 'B';
+		else 
+		  {
+		  if (testValue >= 70.0)
+				grade = 'C';
+          else
+		     {
+			 if (testValue >= 60.0)
+			    grade = 'D';
+			 else
+			    grade = 'F';
+			 }
+		}	
+	}
+    return grade;
+
+}
+
+
+
+int main (void){
+
+    double testValue = 90.0;
+
+    printf("What did you get on your test? \n");
+	scanf("%lf", &testValue);
+	printf("Your test score: %lf\n", testValue);
+
+	printf("Your grade is: %c\n", getGrade(testValue));
+	
+	return 0;
+}
