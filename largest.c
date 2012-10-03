4e631bff63368891eb57095953d60991ad1f3058
Esther N <esthernam@gmail.com>
2012-10-02 23:25:47 -0700

    pseudocode for largest

---
+++ largest.c
@@ -0,0 +1,46 @@
+/*
+
+Largest Int
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define END_CHAR 0
+
+
+
+
+int main (void)
+{
+    int number = -1,  // initialize so that max and number go throguh the do-while loop once
+        max=0,
+        max_count=0;
+    char input;
+
+    printf("Enter a series of numbers; enter 0 to finish: ");
+
+    /*
+     get the input:
+
+     scanf string
+
+
+     parse the input for integers:
+
+
+     for each integer:
+        if integer > max
+            max = integer
+        else
+            if integer == max
+                max_count++
+
+
+
+    */
+
+    return 0;
+}
