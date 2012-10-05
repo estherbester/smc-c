906cb5347f4870e422d18815a0995a2e2531e642
Esther N <esthernam@gmail.com>
2012-10-04 22:54:57 -0700

    filling out dice more

--- dice.c
+++ dice.c
@@ -1,15 +1,68 @@
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <libc.h>
-#include <math.h>
-#include <string.h>
+
 
 #define DIE_SIZE 6
-#define MAX_THROWS 1000
+#define MAX_THROWS 10000
 
 
-int throw_dice(void) {
+//int throw_dice(void);
+
+void stat_the_array(int * rolls);
+
+
+void reset_throw_count(int * throw_count_array) {
+
+    int i;
+
+    for (i=0; i< DIE_SIZE; i++){
+        throw_count_array[i] = 0;
+    }
+
+}
+
+void print_results(int * rolls){
+
+    int i;
+
+    printf ("\nDie was thrown %d times. \n", MAX_THROWS);
+    for (i=0; i< DIE_SIZE; i++){
+        printf ("%d was thrown: %d times \n", i+1, rolls[i]);
+    }
+    printf("\n\n==INFO==\n");
+    stat_the_array(rolls);
+}
+
+void stat_the_array(int rolls[]){
+    printf("Array size of sizeof(*rolls): %lu\n", sizeof(*rolls));
+    printf("Array size of sizeof(rolls): %lu\n", sizeof(rolls));
+    printf("Array size of sizeof*(rolls): %lu\n", sizeof*(rolls));
+    printf("Rolled 1: %d times\n", rolls[0]);
+    //printf("Array size of sizeof(*rolls): %lu\n", sizeof(*rolls));
+}
+
+int main (void)
+{
+
+    int num_throws, throw, rolls[DIE_SIZE];
+
+    srand ( time(NULL) );
+    reset_throw_count(rolls);
+
+    for (num_throws=0; num_throws < MAX_THROWS; num_throws++)
+    {
+        throw = rand() % DIE_SIZE;
+        rolls[throw] += 1 ;
+    }
+
+    print_results(rolls);
+
+    return 0;
+}
+
+
+int throw_dice(void)
+{
 
     int throw;
 
@@ -20,47 +73,3 @@ int throw_dice(void) {
     return throw;
 
 }
-
-void reset_throw_count(int * throw_count_array) {
-
-    int i;
-
-    for (i=0; i< DIE_SIZE; i++){
-        throw_count_array[i] = 0;
-    }
-
-
-}
-
-void print_results(int * rolls){
-
-    int i;
-
-    printf ("Die was thrown %d times. \n", MAX_THROWS);
-    for (i=0; i< DIE_SIZE; i++){
-        printf ("%d was thrown: %d times \n", i+1, rolls[i]);
-    }
-
-
-}
-
-int main (void)
-{
-
-    int num_throws, throw, rolls[DIE_SIZE];
-
-    reset_throw_count(rolls);
-
-    for (num_throws=0; num_throws < MAX_THROWS; num_throws++)
-    {
-        throw = throw_dice();
-        printf("%d", throw);
-        rolls[throw] += 1 ;
-    }
-
-    print_results(rolls);
-
-    return 0;
-}
-
-
