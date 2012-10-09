a963af1b510ebca47fff4e2dca0a49acebc4c4e4
Esther N <esthernam@gmail.com>
2012-10-09 00:08:32 -0700

    Adding serial

---
+++ serial.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SEPARATOR '-'
+
+
+
+    /*
+        validate first input as letter
+        validate length of letter input as 12
+
+        validate second input as digit
+        validate lenght of digit input as 9
+
+        validate third input as letter
+        validate length of letter input as 9
+    */
+
+int validate_string (int * string)
+{
+
+}
+
+int main(void) {
+    int letter,
+        second[9],
+        i= 0; // initialize counter
+
+    char first[12],
+         third[9]; // declaring the letter arrays
+
+    printf("Enter the serial number: ");
+    while (scanf("%d", &letter) != SEPARATOR){
+
+    }
+    // validate_string(&first);
+    return 0;
+}
