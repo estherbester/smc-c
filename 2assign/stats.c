f90f606f54f5d37555442bcf9ecfe1bb229588ce
Esther N <esthernam@gmail.com>
2012-12-10 00:53:33 -0800

    Stats works. now fix input

--- 2assign/stats.c
+++ 2assign/stats.c
@@ -20,28 +20,39 @@ or the end-of-file symbol on your system.
 
 #define MAX_ITEMS 19
 
+// Used to calculate the mode
+struct frequency
+{
+    double * address;
+    int count;
+};
+
+// Menu program functions
 void print_menu(void);
 void exit_program(void);
-void clear_buffer(void);
 void display_user_data(double *, double * data_tracker);
 void get_user_data(double *, double *);
-void clear_buffer(void);
+void run_program(double * array, double * data_tracker);
+//void clear_buffer(void);
 
+// Statistics functions
+double get_data_sum(double *, int);
+double calculate_mean(double * array, double sum, int length);
+double calculate_median(double * array, int array_length);
+struct frequency * calculate_mode(double * array, int);
+void print_mode(struct frequency mode_array[]);
+double calculate_variance(double * array, int array_length, double mean);
+double calculate_std_dev(double variance);
 
-double get_data_sum(double *, double *);
+// Deprecated
 double calculate_highest(double * array, double * data_tracker);
 double calculate_lowest(double * array, double * data_tracker);
-double calculate_mean(double * array, double * data_tracker, double sum);
-double calculate_median(double * array, double * data_tracker);
-double calculate_mode(double * array, double * data_tracker);
-double calculate_variance(double * array, double * data_tracker);
-double calculate_std_dev(double * array, double * data_tracker);
-void run_program(double * array, double * data_tracker);
 
-double * sort_array(double * array, double * data_tracker);
+// Helper functions
+double * sort_array(double * array, double * data_tracker, int length);
 void copy_array(double new_array_to_return[], double original_array[], int length);
 
-
+// Used only for debugging
 void print_array(double * array, int length);
 
 
@@ -99,6 +110,7 @@ void print_menu (void)
     printf("2: Display data:\n");
     printf("\tNumber of items\n\tHighest value\n\tLowest value\n\tMean\n\tMedian\n\tMode\n\tVariance\n\tStandard Deviation\n");
     printf("3: Quit.\n");
+    printf("Enter your choice: ");
 
 }
 
@@ -126,9 +138,12 @@ void get_user_data(double array[], double * data_tracker)
     {
 
         // DURING DEBUG
-        data_point = data_point - 1.00;
 
+        if (i>0 && i<9)
+        {}
 
+        else
+            data_point = data_point - 1.00;
         // fill the array cell denoted by data_tracker pointer with the user's value
         *data_tracker++ = data_point;
         i++;
@@ -147,25 +162,26 @@ void display_user_data(double array[], double * data_tracker)
             lowest,
             mean,
             median,
-            mode,
             variance,
             std_dev;
     double * sorted_array;
+    struct frequency * mode;
     char c;  // dummy character to keep the screen on the stats.
+    int array_length = data_tracker-array;
 
-    sum = get_data_sum(array, data_tracker);
+    sum = get_data_sum(array, array_length);
 
     // Create a new array, which contains sorted values
-    sorted_array = sort_array(array, data_tracker);
+    sorted_array = sort_array(array, data_tracker, array_length);
 
     lowest = sorted_array[0];
-    highest = sorted_array[data_tracker-array-1];
+    highest = sorted_array[array_length-1];
 
-    mean = calculate_mean(array, data_tracker, sum);
-    median = calculate_median(sorted_array, data_tracker);
-    mode = calculate_mode(array, data_tracker);
-    variance = calculate_variance(array, data_tracker);
-    std_dev = calculate_std_dev(array, data_tracker);
+    mean = calculate_mean(array, sum, array_length);
+    median = calculate_median(sorted_array, array_length);
+    mode = calculate_mode(sorted_array, array_length);
+    variance = calculate_variance(array, array_length, mean);
+    std_dev = calculate_std_dev(variance);
 
     printf("\n2. Display Data Stats:\n");
     printf("----------------------");
@@ -174,33 +190,34 @@ void display_user_data(double array[], double * data_tracker)
     printf("\n%10s\t%4.4lf","Lowest:", lowest);
     printf("\n%10s\t%4.4lf","Mean:", mean);
     printf("\n%10s\t%4.4lf","Median:", median);
-    printf("\n%10s\t%4.4lf","Mode:", mode);
+    printf("\n%10s\t","Mode(s):");
+    print_mode(mode);
     printf("\n%10s\t%4.4lf","Variance:", variance);
     printf("\n%10s\t%4.4lf","Std_dev:", std_dev);
     printf("\n----------------------");
     printf("\n");
     printf("Press CTRL+Z to continue ");
+    printf("\n\n");
     fflush(stdout);
     while (scanf("%c", &c) != EOF);
 
-    printf("\n\n");
-
+    run_program(array, data_tracker);
 }
 
-/*
-    STAT FUNCTIONS. The following functions follow this basic model:
-    1. Given the array in params,
-    2. Traverse the array and calculate the sums with their respective formulae
-    3. Return the calculated values
-*/
 
 
-// Calculate the running sum of all the items in the array
-double get_data_sum(double * array, double * data_tracker)
+ /*
+     STATISTICS FUNCTIONS. The following functions follow this basic model:
+     1. Given the array in params,
+     2. Traverse the array and calculate the sums with their respective formulae
+     3. Return the calculated values
+ */
+
+// Given the array values, calculate and return the running total.
+double get_data_sum(double * array, int number_of_items)
 {
     int i=0;
     double running_total = 0;
-    int number_of_items = data_tracker - array;
     for (i; i<number_of_items; i++)
     {
         running_total += array[i];
@@ -209,7 +226,190 @@ double get_data_sum(double * array, double * data_tracker)
     return running_total;
 }
 
-/* The following two functions are deprecated because by sorting
+
+double calculate_mean(double * array, double sum, int number_of_items)
+{
+    return (double) sum/number_of_items;
+}
+
+/*
+   Uses the sorted array to find the midpoint value of the data. If there is
+   an even number of data points, we must return the mean of the two data points
+   in the middle.
+*/
+double calculate_median(double * array, int array_length)
+{
+    int median_point = array_length/2;
+
+    if (array_length % 2)  // evaluates to True if median_point is odd
+        return array[median_point];
+    else  // this means median_point is even
+        return (double) (array[median_point]+array[median_point-1])/2;
+}
+
+
+/*
+    To find the mode, we have to keep track of the frequency of every value in our array.
+    We will traverse our data array, and create an array of pointers to help us record
+    each time we encounter a new value
+
+    To make things easier we will use the sorted array so that equal values appear in succession.
+
+    (struct definition copied here for reference)
+    struct frequency
+    {
+        double * address;
+        int count;
+    };
+
+*/
+struct frequency * calculate_mode(double * sorted_array, int array_length)
+{
+    // Potentially every number is a mode, e.g. if each number is unique!
+    struct frequency * mode_array = malloc(sizeof(struct frequency) * array_length);
+    struct frequency * current_counter = mode_array;  // Start our tracker at the beginning
+
+    int i=0,
+        highest_frequency=0;
+
+    // Initialize our tracker array, mode_array, with the first value from our params
+    current_counter->address = sorted_array;
+    current_counter->count=1;
+
+    do
+    {
+        // If the current array value being tested matches the last item in our tracker array,
+        // then increment that item's counter
+        if (*(++sorted_array) == *(current_counter->address))
+        {
+            (current_counter->count)++;
+        }
+        else  // If it's a new value, create a new entry in the array,
+              // populate it with the address of the current array value,
+              // and start the counter again
+        {
+            current_counter++;
+            current_counter->address = sorted_array;
+            current_counter->count = 1;
+        }
+
+        if ((current_counter->count) > highest_frequency)
+        {
+            // New winner!  Put our tracker pointer back to the beginning of our
+            // tracker array (we want to overwrite old value and start from zero)
+            mode_array[0] = *current_counter;
+            highest_frequency = current_counter->count;
+        }
+        i++;
+    }
+    while (i < array_length); // To make sure we traverse the entire data array.
+
+    return mode_array;
+}
+
+void print_mode(struct frequency mode_array[])
+{
+    struct frequency * current_point = mode_array;
+
+    // Presumably anything that's not the same count old values that were not written over
+    while (current_point->count >= mode_array->count)
+    {
+        printf("%3lf (%d occurrences) ", *(current_point++->address), current_point->count);
+    }
+}
+
+
+/*
+   To get variance:
+   1. Subtract each data point from the mean
+   2. Square each of the differences
+   3. Calculate the sum of the squares
+   4. Divide the sum of the squares by the number of items
+
+   Return {double} variance
+*/
+double calculate_variance(double * array, int array_length, double mean)
+{
+    double sums_of_squares=0;
+    int i;
+
+    for (i=0;i<array_length;i++)
+    {
+        sums_of_squares += pow(array[i]-mean, 2);
+    }
+
+    return (double) sums_of_squares/array_length;
+}
+
+
+// Standard deviation is just the square root of the variance
+double calculate_std_dev(double variance)
+{
+    return pow(variance, .5);
+}
+
+
+/*
+    Returns a pointer to an array representing the sorted copy of the original array
+*/
+double * sort_array(double * array, double * data_tracker, int array_length)
+{
+    double * sorted_array;
+    double temp;
+    int i, j;
+
+    // Allocate memory for the new array
+    sorted_array = malloc(array_length * sizeof(double));
+
+    // Here we copy the array
+    copy_array(sorted_array, array, array_length);
+
+    // Now we sort the values. I forget which sort this is called. We did in class.
+    for (i=0; i < array_length-1; i++)
+    {
+        for (j=i+1; j<array_length; j++)
+        {
+            if (sorted_array[i] > sorted_array[j])
+            {
+                temp = sorted_array[i];
+                sorted_array[i] = sorted_array[j];
+                sorted_array[j] = temp;
+            }
+        }
+
+    }
+
+    return sorted_array;
+}
+
+
+/*
+    Given an array and the length of the array, copies the values into the new array
+*/
+void copy_array(double new_array_to_return[], double original_array[], int length)
+{
+    int i;
+
+    for (i=0; i< length; i++)
+    {
+        new_array_to_return[i] = original_array[i];
+    }
+}
+
+
+// For debugging
+void print_array(double * array, int length)
+{
+    int i=0;
+    while (i<length)
+    {
+        printf("%lf\n ", array[i]);
+        i++;
+    }
+}
+
+/*
+The following two functions are deprecated because by sorting
 the array we can get the highest and lowest by calling the
 last/first values of the sorted array, respectively.
 */
@@ -246,110 +446,4 @@ double calculate_lowest(double * array, double * data_tracker)
     }
 
     return current_lowest;
-}
-
-
-double calculate_mean(double * array, double * data_tracker, double sum)
-{
-    int number_of_items = data_tracker - array;
-
-    return (double) sum/number_of_items;
-}
-
-
-// Uses the sorted array to find the midpoint value of the data. If there is
-// an even number of data points, we must return the mean of the two data points
-// in the middle.
-double calculate_median(double * array, double * data_tracker)
-{
-    int array_length = data_tracker - array;
-    int median_point = array_length/2;
-
-    print_array(array, data_tracker-array);
-
-    if (array_length % 2)  // evaluates to True if median_point is odd
-        return array[median_point];
-    else
-        return (double) (array[median_point]+array[median_point-1])/2;
-}
-
-double calculate_mode(double * array, double * data_tracker)
-{
-    return array[0];
-}
-
-double calculate_variance(double * array, double * data_tracker)
-{
-    return array[0];
-}
-
-double calculate_std_dev(double * array, double * data_tracker)
-{
-    return array[0];
-}
-
-/*
-    Returns a pointer to an array representing the sorted copy of the original array
-*/
-double * sort_array(double * array, double * data_tracker)
-{
-    double * sorted_array;
-    double temp;
-    int number_of_items = data_tracker - array;
-    int i, j;
-
-    // Allocate memory for the new array
-    sorted_array = malloc(number_of_items * sizeof(double));
-    // Here we copy the array
-    copy_array(sorted_array, array, number_of_items);
-
-    // Now we sort the values. I forget which sort this is called. We did in class.
-    for (i=0; i < number_of_items-1; i++)
-    {
-        for (j=i+1; j<number_of_items; j++)
-        {
-            if (sorted_array[i] > sorted_array[j])
-            {
-                temp = sorted_array[i];
-                sorted_array[i] = sorted_array[j];
-                sorted_array[j] = temp;
-            }
-        }
-
-    }
-/*
-    printf("\nDEBUG:\n");
-    printf("\nORIG:\n");
-    print_array(array, number_of_items);
-    printf("\nNEW:\n");
-    print_array(sorted_array, number_of_items);
-*/
-    return sorted_array;
-}
-
-
-/*
-    Given an array and the length of the array, copies the values into the new array
-*/
-void copy_array(double new_array_to_return[], double original_array[], int length)
-{
-    int i;
-
-    for (i=0; i< length; i++)
-    {
-        new_array_to_return[i] = original_array[i];
-    }
-
-}
-
-
-// For debugging
-void print_array(double * array, int length)
-{
-    int i=0;
-    while (i<length)
-    {
-        printf("%lf\n ", array[i]);
-        i++;
-    }
 }
